5de5db8c0040569d1f4fb38805acd8cd8620a416
ziyadanbari <ziyadanbari@gmail.com>
2024-03-14 23:48:23 +0000

    edit public viewing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use("/", express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
